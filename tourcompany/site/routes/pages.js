8735f8179df6f228b9083ad329b4c7b33af98c05
F1zge <arsenijbaulin@yandex.ru>
2024-05-09 14:59:18 +0300

    дописал маршруты

--- tourcompany/site/routes/pages.js
+++ tourcompany/site/routes/pages.js
@@ -53,3 +53,47 @@ router.get('/orders', async function(req, res){
 
 });
 
+router.get('/tour/:id', async function(req, res){
+    let tourId = req.params.id;
+    let tourResponse = await getTourById(tourId);
+    if (tourResponse.code === 404){
+        res.status(404).render('pages/404');
+        return;
+    }
+    let clients = await getAllClients();
+    if (clients.code === 404){
+        res.status(404).render('pages/404');
+        return;
+    }
+    res.status(200).render('pages/admin/tour.ejs', {
+        tour: tourResponse.tour,
+        clients: clients.clients
+    });
+});
+router.get('/statistics', async function(req, res){
+    let toursResponse = await getTours();
+    let tours = toursResponse.tours;
+    let getOrdersResponse = await getAllOrders();
+    let orders = getOrdersResponse.orders;
+    let _, lostMoney = orders.reduce((acc, item) => {
+        let discountMoney = item.price * (item.discount / 100);
+        console.log(discountMoney);
+        return acc + discountMoney;
+    }, 0)
+
+    let saledTours = [...tours.sort((a, b) => b.saled - a.saled)];
+    let amountTours = [...tours.sort((a, b) => b.amount - a.amount)];
+
+    res.status(200).render('pages/admin/statistics.ejs',{
+        saledTours: saledTours,
+        amountTours: amountTours,
+        lostMoney: Math.round(lostMoney)
+    });
+});
+
+router.get('/users', async function(req, res){
+    res.status(200).render('pages/admin/users.ejs');
+
+});
+
+module.exports = router;
